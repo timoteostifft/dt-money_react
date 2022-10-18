7743332c9cb38ad2fb70c21329f9099c3687a025
Timóteo Stifft <timoteostifft@gmail.com>
2022-10-18 19:08:43 -0300

    refactor: move transactions to context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Summary from '../../components/Summary';
+import { Transaction, TransactionsContext } from '../../contexts/TransactionsContext';
 import Search from './components/Search';
 
 import { Container, PriceHighlight, TransactionsTable } from './styles';
 
-interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createdAt: string
-}
-
 const Transactions: React.FC = () => {
 
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
