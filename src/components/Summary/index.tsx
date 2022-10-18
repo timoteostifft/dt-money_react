c09962fd1069a67f5a4961b49484172651b99b9f
Timóteo Stifft <timoteostifft@gmail.com>
2022-10-18 19:43:03 -0300

    feat: dinamic summary display

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Container, Card } from './styles';
 
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 const Summary: React.FC = () => {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {
+    income: 0, outcome: 0, total: 0
+  })
+
   return (
     <Container>
       <Card>
@@ -13,7 +30,7 @@ const Summary: React.FC = () => {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </Card>
 
       <Card>
@@ -22,7 +39,7 @@ const Summary: React.FC = () => {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </Card>
 
       <Card variant='green'>
@@ -31,7 +48,7 @@ const Summary: React.FC = () => {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </Card>
     </Container>
   );
